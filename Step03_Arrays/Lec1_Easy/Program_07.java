7da6a2dac13e9e427d2ce2085576c02b76f551a2
whatisaProCoder <debnathpritam2005@gmail.com>
2024-08-23 14:28:57 +0530

    Init

---
+++ Step03_Arrays/Lec1_Easy/Program_07.java
@@ -0,0 +1,15 @@
+package Step03_Arrays.Lec1_Easy;
+
+import java.util.*;
+
+// Moving zeroes to end 
+
+public class Program_07 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // write code here
+
+        in.close();
+    }
+}
