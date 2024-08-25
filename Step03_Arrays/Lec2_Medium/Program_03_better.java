006fdcaae719d6095d6989328d8de5835956ba70
whatisaProCoder <debnathpritam2005@gmail.com>
2024-08-25 12:15:52 +0530

    Prog03better

--- Step03_Arrays/Lec2_Medium/Program_03_better.java
+++ Step03_Arrays/Lec2_Medium/Program_03_better.java
@@ -1,4 +1,35 @@
 package Step03_Arrays.Lec2_Medium;
 
+import java.util.*;
+
+// Majority Element (>n/2 times)
+// Better : Using Hashing
+
 public class Program_03_better {
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        SortedMap<Integer, Integer> map = new TreeMap<>();
+        // <element, frequency>
+
+        for (int element : nums) {
+            map.put(element, map.getOrDefault(element, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n / 2) {
+                return entry.getKey();
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 4, 2, 4, 3, 4, 4, 3, 2, 4};
+
+        System.out.println("Majority element = " + majorityElement(nums));
+    }
 }
+
+// Time-complexity  : O(N log N + N)
+// Space-complexity : O(N) [for all unique elements]
