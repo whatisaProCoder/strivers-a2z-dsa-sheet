1cd6ff0edbc80165237102641b04c2a8c7c28389
whatisaProCoder <debnathpritam2005@gmail.com>
2024-08-31 22:06:21 +0530

    Prog10optimal

--- Step03_Arrays/Lec2_Medium/Program_10_optimal.java
+++ Step03_Arrays/Lec2_Medium/Program_10_optimal.java
@@ -1,30 +1,49 @@
 package Step03_Arrays.Lec2_Medium;
 
 // Set Matrix Zeros
-// Better Solution
+// Optimal Solution
 
-public class Program_10_better {
+public class Program_10_optimal {
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int[] rowHash = new int[n];
-        int[] colHash = new int[m];
-
+        //  int[] rowHash = new int[n]; -> matrix[..][0]
+        //  int[] colHash = new int[m]; -> matrix[0][..]
+        int column0 = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    rowHash[i] = 1;
-                    colHash[j] = 1;
+                    // mark the i-th row
+                    matrix[i][0] = 0;
+                    // mark the j-th column
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        column0 = 0;
                 }
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (rowHash[i] == 1 || colHash[j] == 1) {
-                    matrix[i][j] = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // check for column & row
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
                 }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (column0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
@@ -40,7 +59,7 @@ public class Program_10_better {
             System.out.println();
         }
 
-        new Program_10_better().setZeroes(matrix);
+        new Program_10_optimal().setZeroes(matrix);
 
         System.out.println("After processing..");
         for (int i = 0; i < matrix.length; i++) {
