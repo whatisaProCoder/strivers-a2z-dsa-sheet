ad228e69ff4613ddef93580b1b2ea48fd9d20793
whatisaProCoder <debnathpritam2005@gmail.com>
2024-10-11 20:20:52 +0530

    P12

--- Step03_Arrays/Lec2_Medium/Program_12.cpp
+++ Step03_Arrays/Lec2_Medium/Program_12.cpp
@@ -3,11 +3,48 @@ using namespace std;
 
 // Print the matrix in spiral manner
 
-vector<int> spiralOrder_BRUTE(vector<vector<int>> &matrix) {}
+// Time-complexity  : O(n * m)
+// Space-complexity : O(n * m) [for storing the answer]
+vector<int> spiralOrder_OPTIMAL(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
 
-vector<int> spiralOrder_BETTER(vector<vector<int>> &matrix) {}
+    int left = 0, right = m - 1;
+    int top = 0, bottom = n - 1;
 
-vector<int> spiralOrder_OPTIMAL(vector<vector<int>> &matrix) {}
+    vector<int> ans;
+
+    while (top <= bottom && left <= right) {
+        // right
+        for (int i = left; i <= right; i++) {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+
+        // bottom
+        for (int i = top; i <= bottom; i++) {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        // left
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        // top
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return ans;
+}
 
 int main() {
     int n;
@@ -31,6 +68,6 @@ int main() {
 
     cout << "In spiral order..." << endl;
     for (auto element : spiralOrder_OPTIMAL(matrix)) {
-        cout << element << endl;
+        cout << element << " ";
     }
 }
