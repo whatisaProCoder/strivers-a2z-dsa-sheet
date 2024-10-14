65767f7a41e5dc4baeb6da5b417fd621447000e4
whatisaProCoder <debnathpritam2005@gmail.com>
2024-10-15 01:32:14 +0530

    Java_INIT

--- Step03_Arrays/Lec3_Hard/Program_06.cpp
+++
@@ -1,4 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// Count number of subarrays with given xor K
---
+++ Step03_Arrays/Lec3_Hard/Program_06.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+// Count number of subarrays with given xor K
+
+class Program_06 {
+    public static void main(String[] args) {
+
+    }
+}
