0f1b674ee5ca67c38159b944c9eab0b96e4fc6d4
whatisaProCoder <debnathpritam2005@gmail.com>
2024-10-17 18:52:48 +0530

    P8

--- Step03_Arrays/Lec3_Hard/Program_08.cpp
+++ Step03_Arrays/Lec3_Hard/Program_08.cpp
@@ -25,6 +25,59 @@ void merge_BRUTE(vector<int>& nums1, int n, vector<int>& nums2, int m) {
 // Time-complexity  : O(n + m) + O(n + m)
 // Space-complexity : O(n + m)
 
+void merge_OPTIMAL1(vector<int>& nums1, int n, vector<int>& nums2, int m) {
+    int left = n - 1;
+    int right = 0;
+    while (left >= 0 && right < m) {
+        if (nums1[left] > nums2[right]) {
+            swap(nums1[left], nums2[right]);
+            left--;
+            right++;
+        } else
+            break;
+    }
+    sort(nums1.begin(), nums1.begin() + n);
+    sort(nums2.begin(), nums2.begin() + m);
+}
+// Time-complexity  : O(min(n, m)) + O(n log n) + O(m log m)
+// Space-complexity : O(1)
+
+// Gap Method, Shell Sorting Algorithm
+void swap_if_greater(vector<int>& nums1, vector<int>& nums2, int ind1,
+                     int ind2) {
+    if (nums1[ind1] > nums2[ind2]) swap(nums1[ind1], nums2[ind2]);
+}
+void merge_OPTIMAL2(vector<int>& nums1, int n, vector<int>& nums2, int m) {
+    int len = n + m;
+    int gap = (len / 2) + (len % 2);
+    while (gap > 0) {
+        int left = 0;
+        int right = left + gap;
+        while (right < len) {
+            // left's in nums1 and right's in nums2
+            if (left < n && right >= n) {
+                swap_if_greater(nums1, nums2, left, right - n);
+            }
+            // left and right, both are in nums2
+            else if (left >= n) {
+                swap_if_greater(nums2, nums2, left - n, right - n);
+            }
+            // left and right, both are in nums1
+            else {
+                swap_if_greater(nums1, nums1, left, right);
+            }
+            left++;
+            right++;
+        }
+        if (gap == 1)
+            break;
+        else
+            gap = (gap / 2) + (gap % 2);
+    }
+}
+// Time-complexity  : O(log_2(n + m)) + O(n + m)
+// Space-complexity : O(1)
+
 int main() {
     int n;
     cin >> n;
@@ -35,7 +88,7 @@ int main() {
     vector<int> nums2(m);
     for (int i = 0; i < m; i++) cin >> nums2[i];
 
-    merge_BRUTE(nums1, n, nums2, m);
+    merge_OPTIMAL2(nums1, n, nums2, m);
 
     for (int ele : nums1) cout << ele << " ";
     for (int ele : nums2) cout << ele << " ";
